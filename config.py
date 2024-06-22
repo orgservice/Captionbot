36d50d215f2699393c54ecdff4537dcefe4af1de
orgservice <146081596+orgservice@users.noreply.github.com>
2024-06-22 23:20:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
     ADMIN_USERNAME = "adultsupport"  # without "@". 
 
     # a list of strings of words to remove from the existing caption
-    WORDS_TO_REMOVE = ["🔸 Upload By [@The_Silent_Teams]", "⚡️Join:- [@ViSHWA_MOViEX]", "©@piro_files & 🤖@piroxbots", "@piro_files", "@piroxbots", "⚡ Join :- [@flimyworld_17]", "💥Join:- @ViSHWA_MOViEX", "🍿 𝖩𝗈𝗂𝗇  ➥ 「 @RJMoviessWorld 」"]  
+    WORDS_TO_REMOVE = ["💥Join:- [@ViSHWA_MOViEX]","🔸 Upload By [@The_Silent_Teams]", "⚡️Join:- [@ViSHWA_MOViEX]", "©@piro_files & 🤖@piroxbots", "@piro_files", "@piroxbots", "⚡ Join :- [@flimyworld_17]", "💥Join:- @ViSHWA_MOViEX", "🍿 𝖩𝗈𝗂𝗇  ➥ 「 @RJMoviessWorld 」"]  
     # a list of regex pattern strings to remove from the existing caption. 
     # For eg. r".*Join.*" will remove the entire line having word Join
     REGEX_PATTERNS = []  
