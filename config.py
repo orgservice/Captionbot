896d0ec8fc6b6143735c6881836346e64f8916db
orgservice <146081596+orgservice@users.noreply.github.com>
2026-02-25 17:14:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     # Fetching secrets from environment variables
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8591161370:AAEhWSq_SA-eApXE1UC03TCdVaVzle4POYI")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8591161370:AAHVltcHDBLMtDq4DFsxg2mQEsdn4zMZJP0")
     API_ID = int(os.environ.get("API_ID", "25465082"))
     API_HASH = os.environ.get("API_HASH", "4a6b5e40c8bc08c8af09add6cca23b18")
     
